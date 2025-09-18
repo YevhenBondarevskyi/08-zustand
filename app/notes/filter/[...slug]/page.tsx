d6fabb531bcd552f793d65fb033b2222c2658538
Yevhen <bondarevskiy@gmail.com>
2025-09-18 09:19:05 +0200

    hw8nj

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,11 +5,34 @@ import {
 } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api";
+import { Metadata } from "next";
 
 type Props = {
   params: Promise<{ slug?: string[] }>;
 };
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const resolvedParams = await params;
+  const slug = resolvedParams.slug ?? [];
+  const tag = slug[0] === "all" ? undefined : slug[0];
+  return {
+    title: `${tag} notes`,
+    description: `All ${tag} notes`,
+    openGraph: {
+      title: `${tag} notes`,
+      description: `All ${tag} notes`,
+      url: `https://08-zustand-ten-ochre.vercel.app/notes/filter/${tag}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Note Hub Logo",
+        },
+      ],
+    },
+  };
+}
 export default async function NotesPage({ params }: Props) {
   const resolvedParams = await params;
   const slug = resolvedParams.slug ?? [];
